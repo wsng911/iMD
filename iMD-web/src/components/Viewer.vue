45691037441b69271c2401bb849bc7b495fb7f38
wsng911 <824559662@qq.com>
2026-04-14 10:11:12 +0800

    v1.2.8: drawer right side, fab sticky right

--- iMD-web/src/components/Viewer.vue
+++ iMD-web/src/components/Viewer.vue
@@ -8,20 +8,23 @@
       </button>
     </div>
     <div class="viewer-body">
-      <!-- 大纲抽屉（桌面端撑开布局，手机端覆盖） -->
-      <div class="outline-drawer" :class="{ open: drawerOpen }">
-        <div class="outline-drawer-title">大纲</div>
-        <div v-for="h in headings" :key="h.id" :class="['outline-drawer-item', `lv${h.level}`]" @click="jumpTo(h); drawerOpen = false">{{ h.text }}</div>
-      </div>
-      <div class="outline-drawer-mask" v-if="drawerOpen" @click="drawerOpen = false" />
       <div class="viewer-scroll">
         <div class="md-wrap" ref="mdWrap" @click="handleClick" @dblclick="handleDblClick">
           <MdPreview :modelValue="content" :theme="settings.theme" :showCodeRowNumber="true" />
         </div>
-        <button v-if="headings.length" class="outline-fab" @click="drawerOpen = !drawerOpen">
+      </div>
+      <template v-if="headings.length">
+        <!-- fab 固定在右侧 -->
+        <button class="outline-fab" @click="drawerOpen = !drawerOpen">
           <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><line x1="3" y1="6" x2="21" y2="6"/><line x1="3" y1="12" x2="15" y2="12"/><line x1="3" y1="18" x2="12" y2="18"/></svg>
         </button>
-      </div>
+        <!-- 抽屉从右侧向左弹出 -->
+        <div class="outline-drawer" :class="{ open: drawerOpen }">
+          <div class="outline-drawer-title">大纲</div>
+          <div v-for="h in headings" :key="h.id" :class="['outline-drawer-item', `lv${h.level}`]" @click="jumpTo(h); drawerOpen = false">{{ h.text }}</div>
+        </div>
+        <div class="outline-drawer-mask" v-if="drawerOpen" @click="drawerOpen = false" />
+      </template>
     </div>
   </div>
 </template>
