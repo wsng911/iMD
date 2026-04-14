8512a011d55f0ae8a97416a774be202334dd0282
wsng911 <824559662@qq.com>
2026-04-14 11:13:39 +0800

    v1.2.9: mobile fab larger+shifted, back btn text

--- iMD-web/src/components/Viewer.vue
+++ iMD-web/src/components/Viewer.vue
@@ -6,9 +6,7 @@
         <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
         编辑
       </button>
-      <button class="back-home-btn" @click="$emit('back')">
-        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><polyline points="15 18 9 12 15 6"/></svg>
-      </button>
+      <button class="back-home-btn" @click="$emit('back')">返回</button>
     </div>
     <div class="viewer-body">
       <div class="viewer-scroll">
