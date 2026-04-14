2c5399853d1185ddb8a55a00df3c605241bee044
wsng911 <824559662@qq.com>
2026-04-14 10:49:33 +0800

    v1.2.8: fab above drawer, z-index fix, fab follows drawer

--- iMD-web/src/components/Viewer.vue
+++ iMD-web/src/components/Viewer.vue
@@ -15,7 +15,7 @@
       </div>
       <template v-if="headings.length">
         <!-- fab 固定在右侧 -->
-        <button class="outline-fab" @click="drawerOpen = !drawerOpen">
+        <button class="outline-fab" :class="{ open: drawerOpen }" @click="drawerOpen = !drawerOpen">
           <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><line x1="3" y1="6" x2="21" y2="6"/><line x1="3" y1="12" x2="15" y2="12"/><line x1="3" y1="18" x2="12" y2="18"/></svg>
         </button>
         <!-- 抽屉从右侧向左弹出 -->
