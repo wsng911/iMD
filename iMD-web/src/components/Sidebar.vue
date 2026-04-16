7cbcd31632581991411466652e60c4c82f1dfb4d
wsng911 <824559662@qq.com>
2026-04-16 16:04:10 +0800

    v2.3: sidebar 220px, iMD logo, doclist flex:1, main flex:4

--- iMD-web/src/components/Sidebar.vue
+++ iMD-web/src/components/Sidebar.vue
@@ -1,6 +1,7 @@
 <template>
   <aside :class="['sidebar', { collapsed }]" ref="sidebarRef">
     <div class="sidebar-header">
+      <span class="sidebar-logo">iMD</span>
       <div class="header-actions">
         <button class="icon-btn" title="新建文件夹" @click="newGroup">
           <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/><line x1="12" y1="11" x2="12" y2="17"/><line x1="9" y1="14" x2="15" y2="14"/></svg>
