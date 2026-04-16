b765ec08c2fa10e78867d02381c62918fce85ef2
wsng911 <824559662@qq.com>
2026-04-16 18:08:17 +0800

    v2.3: marquee title on hover, fix newDocInput residual

--- iMD-web/src/components/Sidebar.vue
+++ iMD-web/src/components/Sidebar.vue
@@ -11,7 +11,7 @@
 
     <div class="sidebar-middle">
       <!-- 文件树 -->
-      <div class="tree" ref="treeRef">
+      <div class="tree" ref="treeRef" @click.self="newDocInput = null; newGroupInput = false">
         <div v-if="newGroupInput" class="inline-input-row">
           <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/></svg>
           <input ref="groupInputRef" v-model="newGroupName" placeholder="文件夹名称"
@@ -257,7 +257,6 @@ function confirmNewDoc(gid) {
   emit('update:docs', props.docs.map(g => g.id === gid ? { ...g, children: [...g.children, { id, title, content: '' }] } : g))
   emit('new-doc', { id, title, content: '' })
 }
-
 const time = ref('')
 let timer = null
 function updateTime() { time.value = new Date().toLocaleTimeString('zh-CN', { hour: '2-digit', minute: '2-digit', second: '2-digit' }) }
